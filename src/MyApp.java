1ee4743f99609c7026064f835f986bd83717286e
BerArth <arthur.bertrand16@hotmail.fr>
2025-03-05 08:48:13 +0100

    add MyApp and config vue

---
+++ src/MyApp.java
@@ -0,0 +1,27 @@
+import Vue_editeur.EditeurPane;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MyApp extends Application{
+    
+    @Override
+    public void start (Stage stage) throws Exception {
+        
+        final EditeurPane myPane = new EditeurPane();
+
+        // Scene dimensions
+        double width = 850.0, height = 600.0;
+
+        // Scene = container for all content
+        Scene scene = new Scene(myPane, width, height);
+        stage.setScene(scene);
+        stage.setTitle("Editeur de niveau");
+        stage.show();  
+    }
+    
+    public static void main (String args []) {
+		launch(args);
+    }
+
+}
---
+++ src/view/viewConfig/ConfigView.java
@@ -0,0 +1,9 @@
+package view.viewConfig;
+
+import javafx.stage.Stage;
+
+public class ConfigView extends Stage{
+    
+    
+
+}
