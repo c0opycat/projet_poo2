2e3c66a03cea961c71ddaa869062aacf6d86dcfe
Leila <leila.cooper@etu.univ-poitiers.fr>
2025-03-08 18:50:25 +0100

    view hall of fame

---
+++ src/view/viewHallOfFame/HOFView.java
@@ -0,0 +1,51 @@
+package view.viewHallOfFame;
+
+import java.util.ArrayList;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.paint.Color;
+import view.BorderWithButtons;
+
+public class HOFView extends BorderWithButtons{
+
+    public HOFView()
+    {
+        super(createButtons());
+
+        //Adding the title at the top of the pane.
+        Label title = new Label("HALL OF FAME");
+        
+        this.setTop(title);
+
+        //Adding the scores in the center of the pane.
+        ScoresView scores = new ScoresView();
+        this.setCenter(scores);
+
+
+        //Style
+        title.setStyle("-fx-font-weight: bold;");
+        title.setPadding(new Insets(10));
+        title.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, null, null)));
+        BorderPane.setAlignment(title, Pos.CENTER);
+        scores.setAlignment(Pos.CENTER);
+        BorderPane.setMargin(title, new Insets(10));
+        BorderPane.setMargin(scores, new Insets(2));
+    }
+
+    public static ArrayList<Button> createButtons()
+    {
+        ArrayList<Button> buttons = new ArrayList<>();
+
+        Button menu = new Button("Menu");
+        buttons.add(menu);
+
+        return buttons;
+    }
+}
--- src/view/viewHallOfFame/ScoresView.java
+++ src/view/viewHallOfFame/ScoresView.java
@@ -1,12 +1,63 @@
 package view.viewHallOfFame;
 
+import java.util.ArrayList;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 
 public class ScoresView extends VBox{
     public ScoresView()
     {
         super();
+
+        this.addScores();
     }
 
-    public scoresDisplay()
+    private void addScores()
+    {
+        //Récupérer les meilleurs scores (avec pseudo ?)
+        ArrayList<Integer> scores = new ArrayList<>(); //Exemple
+
+        scores.add(15);
+        scores.add(11);
+        scores.add(9);
+        scores.add(7);
+        scores.add(5);
+
+        int ind = 1; //Place of the score 
+        
+        for(Integer score : scores)
+        {
+            
+            if(ind > 10) //Peut être pas nécessaire selon les scores dans le model
+            {
+                break;
+            }
+
+            HBox scoreBox = new HBox();
+
+            Label indLabel = new Label("#" + ind + " : ");
+            Region spring = new Region();
+            Label scoreLabel = new Label("" + score);
+
+            scoreBox.getChildren().addAll(indLabel, spring, scoreLabel);
+
+            //Style
+            scoreBox.setMaxWidth(100);
+            HBox.setHgrow(spring, Priority.ALWAYS);
+            indLabel.setAlignment(Pos.CENTER_LEFT);
+            scoreLabel.setAlignment(Pos.CENTER_RIGHT);
+            HBox.setMargin(scoreLabel, new Insets(2));
+            scoreBox.setAlignment(Pos.CENTER);
+
+            this.getChildren().add(scoreBox);
+
+            ind++;
+        }
+    }
 }
