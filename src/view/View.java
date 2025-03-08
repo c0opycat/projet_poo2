e8d1f17a0fa6bc07fd7daf9d5af6b6f88fc1d477
Leila <leila.cooper@etu.univ-poitiers.fr>
2025-03-08 18:49:48 +0100

    temporary main scene

--- src/view/View.java
+++ src/view/View.java
@@ -1,5 +1,19 @@
 package view;
 
-public class View {
-    
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+
+//Main scene
+public class View extends Scene{
+    public View(Parent root, double width, double height)
+    {
+        super(root, width, height);
+    }
+
+    //Returns the quit button of the root
+    public Button getQuitButton()
+    {
+        return (Button)((BorderWithButtons)this.getRoot()).getQuitButton();
+    }
 }
