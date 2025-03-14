1925fde056856d1c57c781378ba387eb0bb2a81c
BerArth <arthur.bertrand16@hotmail.fr>
2025-03-14 10:13:36 +0100

    deb menu

--- src/view/viewMenu/Menu.java
+++ src/view/viewMenu/Menu.java
@@ -1,7 +1,33 @@
 package view.viewMenu;
 
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
 import view.BorderWithButtons;
 
 public class Menu extends BorderWithButtons{
     
+    public Menu()
+    {
+        super();
+
+        this.addcomp();
+
+    }
+
+    private void addcomp()
+    {
+        VBox vb = new VBox();
+
+        Button jouer = new Button("Play");
+        Button cfg = new Button("Config");
+        Button edit = new Button("Editor");
+        Button hof = new Button("Hall of Fame");
+
+        vb.getChildren().addAll(jouer, cfg, edit, hof);
+
+        this.setCenter(vb);
+
+    
+    }
+
 }
