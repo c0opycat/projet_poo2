47a260cd0c2c3fdfa8d5887db6d5909712a10e59
Gabriel <loki.gabriel.jardin@gmail.com>
2025-03-26 18:12:26 +0100

    Update LocationC.java

--- src/controller/locationC/LocationC.java
+++ src/controller/locationC/LocationC.java
@@ -2,11 +2,14 @@ package controller.locationC;
 import model.location.LocationM;
 import view.viewLocation.LocationView;
 
+import javax.lang.model.element.Name;
+
 public class LocationC {
     public LocationM locationM;
     public LocationView locationView;
 
-    public LocationC() {
+    public LocationC(String name) {
+        this.locationM = new LocationM(new Name());
         
     }
 }
