3657830671887d25059d88d6ef1c9f9358d2b7ff
c0opycat <llcooper37@gmail.com>
2025-05-01 19:32:24 +0200

    fix score save

--- src/model/modelGame/modelScore/ScoreSaveModel.java
+++ src/model/modelGame/modelScore/ScoreSaveModel.java
@@ -1,74 +1,77 @@
 package model.modelGame.modelScore;
 
+import java.io.*;
+import java.util.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.io.*;
-import java.util.*;
-
 public class ScoreSaveModel {
-    private static final String FILE_NAME = "scores.json";
-    private static final int MAX_SCORES = 10;
-    private List<ScoreModel> scores;
 
-    public ScoreSaveModel() {
-        this.scores = new ArrayList<>();
-        loadScores();
-    }
-    private void loadScores() {
-        File file = new File(FILE_NAME);
-        if (!file.exists()) {
-            return; // No scores yet
-        }
+  private static final String FILE_NAME = "./save/scores.json";
+  private static final int MAX_SCORES = 10;
+  private List<ScoreModel> scores;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            StringBuilder json = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                json.append(line);
-            }
+  public ScoreSaveModel() {
+    this.scores = new ArrayList<>();
+    loadScores();
+  }
 
-            JSONArray array = new JSONArray(json.toString());
-            for (int i = 0; i < array.length(); i++) {
-                JSONObject obj = array.getJSONObject(i);
-                scores.add(new ScoreModel(obj.getString("name"), obj.getInt("score")));
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  private void loadScores() {
+    File file = new File(FILE_NAME);
+    if (!file.exists()) {
+      return; // No scores yet
     }
 
-    private void saveScores() {
-        JSONArray array = new JSONArray();
-        for (ScoreModel s : scores) {
-            JSONObject obj = new JSONObject();
-            obj.put("name", s.name);
-            obj.put("score", s.score);
-            array.put(obj);
-        }
+    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+      StringBuilder json = new StringBuilder();
+      String line;
+      while ((line = reader.readLine()) != null) {
+        json.append(line);
+      }
 
-        try (FileWriter writer = new FileWriter(FILE_NAME)) {
-            writer.write(array.toString(4)); // Pretty print with indent
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+      JSONArray array = new JSONArray(json.toString());
+      for (int i = 0; i < array.length(); i++) {
+        JSONObject obj = array.getJSONObject(i);
+        scores.add(new ScoreModel(obj.getString("name"), obj.getInt("score")));
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    public void addScore(String name, int score) {
-        scores.add(new ScoreModel(name, score));
-        scores.sort((a, b) -> Integer.compare(b.score, a.score));
-        if (scores.size() > MAX_SCORES) {
-            scores = scores.subList(0, MAX_SCORES);
-        }
-        saveScores();
+  }
+
+  private void saveScores() {
+    JSONArray array = new JSONArray();
+    for (ScoreModel s : scores) {
+      JSONObject obj = new JSONObject();
+      obj.put("name", s.name);
+      obj.put("score", s.score);
+      array.put(obj);
     }
-    public List<ScoreModel> getScores() {
-        return scores;
+
+    try (FileWriter writer = new FileWriter(FILE_NAME)) {
+      writer.write(array.toString(4)); // Pretty print with indent
+    } catch (IOException e) {
+      e.printStackTrace();
     }
-    public void displayScores() {
-        System.out.println("Best Scores :\n");
-        for (ScoreModel s : scores) {
-            System.out.println(s.name + " - " + s.score+"\n");
-        }
+  }
+
+  public void addScore(String name, int score) {
+    scores.add(new ScoreModel(name, score));
+    scores.sort((a, b) -> Integer.compare(b.score, a.score));
+    if (scores.size() > MAX_SCORES) {
+      scores = scores.subList(0, MAX_SCORES);
     }
+    saveScores();
+  }
+
+  public List<ScoreModel> getScores() {
+    return scores;
+  }
+
+  public void displayScores() {
+    System.out.println("Best Scores :\n");
+    for (ScoreModel s : scores) {
+      System.out.println(s.name + " - " + s.score + "\n");
+    }
+  }
 }
